96af235b82d41b26395f74409778baada61bb4a4
Aekkasitrun <aekkasitrun@gmail.com>
2022-11-23 16:09:49 +0700

    complete connect database1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:testapp1/screen/formscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -51,8 +45,12 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blue,
         bottomNavigationBar: TabBar(
           tabs: [
-            Tab(text: "บันทึกคะแนนสอบ",),
-            Tab(text: "รายชื่อนักเรียน",  )
+            Tab(
+              text: "บันทึกคะแนนสอบ ",
+            ),
+            Tab(
+              text: "รายชื่อนักเรียน",
+            )
           ],
         ),
       ),
--- lib/screen/formscreen.dart
+++ lib/screen/formscreen.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:testapp1/model/student.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:testapp1/model/student.dart';
 
 class FormScreen extends StatefulWidget {
   @override
@@ -11,129 +11,70 @@ class FormScreen extends StatefulWidget {
 
 class _FormScreenState extends State<FormScreen> {
   final formKey = GlobalKey<FormState>();
-  Student myStudent = Student();
-  //เตรียม firebase
-  final Future<FirebaseApp> firebase = Firebase.initializeApp();
-  CollectionReference _studentCollection = FirebaseFirestore.instance.collection("students");
+  late Student myStudent = Student();
+
+  CollectionReference _studentCollection =
+      FirebaseFirestore.instance.collection("students");
+
+      
+  Future<void> addUser(Student myStudent) {
+    // Call the user's CollectionReference to add a new user
+    return _studentCollection
+        .add({
+         "fname": myStudent.fname,
+        })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: firebase,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Scaffold(
+    return Scaffold(
               appBar: AppBar(
-                title: Text("Error"),
-              ),
-              body: Center(
-                child: Text("${snapshot.error}"),
-              ),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text("แบบฟอร์มบันทึกคะแนนสอบ"),
+                title: Text("แบบฟอร์มบันทึกคะแนนสอบ cf"),
               ),
               body: Container(
                 padding: EdgeInsets.all(20),
                 child: Form(
-                  key: formKey,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "ชื่อ",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        TextFormField(
-                          validator: RequiredValidator(
-                              errorText: "กรุณาป้อนชื่อด้วยครับ ^^"),
-                          onSaved: (String? fname) {
-                            myStudent.fname = fname;
-                          },
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          "นามสกุล",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        TextFormField(
-                          validator: RequiredValidator(
-                              errorText: "กรุณาป้อนนามสกุลด้วยครับ ^^"),
-                          onSaved: (String? lname) {
-                            myStudent.lname = lname;
-                          },
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          "อีเมล",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        TextFormField(
-                          validator: MultiValidator([
-                            EmailValidator(errorText: "รูปแบบอีเมลไม่ถูกต้อง"),
-                            RequiredValidator(
-                                errorText: "กรุณาป้อนอีเมลด้วยครับ ^^")
-                          ]),
-                          onSaved: (String? email) {
-                            myStudent.email = email;
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          "คะแนน",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        TextFormField(
-                          validator: RequiredValidator(
-                              errorText: "กรุณาป้อนคะแนนด้วยครับ ^^"),
-                          onSaved: (String? score) {
-                            myStudent.score = score;
-                          },
-                          keyboardType: TextInputType.number,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                              child: Text(
-                                "บันทึกข้อมูล",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              onPressed: () async{
-                                if (formKey.currentState!.validate()) {
-                                   formKey.currentState?.save();
-                                   await _studentCollection.add({
-                                      "fname":myStudent.fname,
-                                      "lname":myStudent.lname,
-                                      "email":myStudent.email,
-                                      "score":myStudent.score
-                                   });
-                                   formKey.currentState?.reset();
-                                }
-                              }),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                    key: formKey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "ชื่อ",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          TextFormField(
+                            onSaved: (String? fname) {
+                              myStudent.fname = fname;
+                            },
+                          ),
+                  
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                                child: Text(
+                                  "บันทึกข้อมูล",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                onPressed: () async {
+                                  print("-----------------------------------------------------------------------");
+                                 
+                                    formKey.currentState?.save();
+                                    
+                                    await addUser(myStudent);
+                              
+                                   
+                                    formKey.currentState?.reset();
+                                  
+                                }),
+                          )
+                        ],
+                      ),
+                    )),
               ),
             );
           }
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        });
-  }
+  
 }

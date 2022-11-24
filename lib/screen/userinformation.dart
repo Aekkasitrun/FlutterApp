6da3b18d92f116d37ce1522356d2b27762d593f0
Aekkasitrun <aekkasitrun@gmail.com>
2022-11-24 12:51:02 +0700

    non read data

---
+++ lib/screen/userinformation.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+ Stream collectionStream = FirebaseFirestore.instance.collection('students').snapshots();
+ Stream documentStream = FirebaseFirestore.instance.collection('students').doc('fname').snapshots();
+
+class UserInformation extends StatefulWidget {
+  @override
+    _UserInformationState createState() => _UserInformationState();
+}
+
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _studentsStream = FirebaseFirestore.instance.collection('students').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _studentsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text("data['fname']"),
+              //subtitle: Text(data['company']),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
